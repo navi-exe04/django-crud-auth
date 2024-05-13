279a4608c3a48d9c2c3138e40f34a667e1109cb4
raul_langle <ra04navi@outlook.com>
2024-05-13 13:33:16 -0600

    feat: completed tasks view is ready

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('signup/', views.signup, name="signup"),
     path('tasks/', views.tasks, name="tasks"),
+    path('tasks_completed/', views.task_completed, name="tasks_completed"),
     path('tasks/create', views.create_task, name="create_task"),
     path('tasks/<int:id>', views.task_detail, name="task_detail"),
     path('tasks/complete/<int:id>', views.complete_task, name="complete_task"),
